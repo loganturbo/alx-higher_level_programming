eb9a24d86b3495d22629da42b1a0b416b489e50f
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2023-01-04 17:42:31 -0800

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
-i#!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-        print("{}".format(chr(i)), end="")
+#!/usr/bin/python3
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
