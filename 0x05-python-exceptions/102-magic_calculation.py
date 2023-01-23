d55945bc1471a0356fa31c364aa908a3783bfffe
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2023-01-23 08:43:46 -0800

    102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,8 +7,9 @@ def magic_calculation(a, b):
         try:
             if i > a:
                 raise Exception('Too far')
+            else:
 
-             result += a ** b / 
+             result += a ** b / i
         except:
             result = b + a
             break
