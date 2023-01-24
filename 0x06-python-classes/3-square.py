8e52103220e3ad4f864e054eb574554ca7a860cb
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2023-01-24 08:27:43 -0800

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
         """
         initializes square
         Args:
-            size: size of side of square
+        size: size of side of square
         """
 
          if type(size) is int:
@@ -17,15 +17,15 @@ class Square:
                  raise ValueError('size must be >= 0')
              else:
                  self.__size = size
-        else:
-            raise TypeError('size must be an integer')
+             else:
+                 raise TypeError('size must be an integer')
 
 
     def area(self):
         """
         finds area of square
         Returns:
-            the area of the square
+        the area of the square
         """
 
         return self.__size ** 2
