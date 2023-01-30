97238ddd6994fb72fc4074c532fe01101da638a6
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2023-01-30 07:56:14 -0800

    2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,13 @@ Defines a class Rectangle
 
 class Rectangle:
     """Representation of a rectangle"""
+
+
      def __init__(self, width=0, height=0):
           """Initializes the rectangle"""
+
+          
+
            self.height = height
            self.width = width
 
